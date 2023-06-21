c2f631bba5d3185b50353f5d7abe60a5c9df6d23
dataanalystg <antonybuuri@gmail.com>
2023-06-21 13:43:03 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+# include <studio.h>
+/**
+ * main program that outputs statement using puts
+ * Return (0) (Success)
+ * /
+int main (void)
+{
+puts ("/"Programming is like building a multilingual puzzle");
+return (o);
+}
