a91e581f357ae43f75b1cff764bbe50d86846ef8
dataanalystg <antonybuuri@gmail.com>
2023-06-24 16:54:40 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,11 +3,11 @@
  * print-alphabet - prints the alphabet in lowercase followed by a new line
  * Return: void
  */
-void print_alphabet(void);
+void print_alphabet(void)
 {
-	char letter = '97';
+	char letter = 97;
 
-	while (letter < = '122')
+	while (letter < = 122)
 	{
 		_putchar(letter);
 	}
