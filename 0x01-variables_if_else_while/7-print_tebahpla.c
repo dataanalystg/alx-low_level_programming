25dc7d49aaf2d6db3a81d2ffd7b668445ca5043d
dataanalystg <antonybuuri@gmail.com>
2023-06-29 14:56:34 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	char g;
+
+	for (g = 'z'; g >= 'a'; g--)
+	{
+		putchar(g);
+	}
+	putchar('\n');
+	return (0);
+}
